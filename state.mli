b0f06347f43eb4a969e87d601a86975f4b4ae1d6
Nobonita Paul <nrp42@cornell.edu>
2019-11-04 13:33:42 -0500

    Added state types to mli

---
+++ state.mli
@@ -0,0 +1,11 @@
+(** The type for the color of a piece. *)
+type color
+
+(** The type for the (x,y) coordinate position on the board. *)
+type position
+
+(** The type for the status of a position on the board. *)
+type status
+
+(** The type for a board. *)
+type board
