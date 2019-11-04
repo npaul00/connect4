108b73924d3f7072753ef36575ce79807a3261c0
Nobonita Paul <nrp42@cornell.edu>
2019-11-04 13:43:07 -0500

    Added required functions

--- state.ml
+++ state.ml
@@ -2,3 +2,12 @@ type color = Red | Black
 type position = int * int
 type status = color option
 type board = (position * status) list
+
+let display b =
+  failwith "Unimplemented"
+
+let move b c col = 
+  failwith "Unimplemented"
+
+let check_win b col =
+  failwith "Unimplemented"
--- state.mli
+++ state.mli
@@ -9,3 +9,14 @@ type status
 
 (** The type for a board. *)
 type board
+
+(** [display b] displays the board [b]. *)
+val display : board -> unit
+
+(** [move b c col] is the state of board [b] after a player with color [col] 
+    puts a piece into column [c]. *)
+val move : board -> int -> color -> board
+
+(** [check_win b col] checks if there are four pieces with color [col] in a row.
+*)
+val check_win : board -> color -> bool
