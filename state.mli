9f46b7817338617125f65a694bcbfd4a773aab9c
Emily Proulx <erp78@cornell.edu>
2019-11-05 12:37:56 -0500

    Fix response to full columns, edit invalid command messages, add documentation

--- state.mli
+++ state.mli
@@ -13,31 +13,36 @@ type board
 (** The abstract type of values representing the game state. *)
 type t
 
+(** [turn t] is the color of whose turn it is to play in state [t]. *)
 val turn : t -> color
 
+(** [board t] is the game board of state [t]. *)
 val board : t -> board
 
+(** [init_state] is the initial state of the game, with no pieces on the board, 
+    and it being Black's turn to play. *)
 val init_state : t
 
-(** [display b] displays the board [b]. *)
+(** [display b i] displays the board [b]. *)
 val display : board -> int -> unit
 
 (** [move t c] is the game state after the current player places a piece into
     column [c] in the current state [t]. *)
 val move : t -> int -> t
 
-(** [check_win b col] checks if there are four pieces with color [col] in a row.
-*)
+(** [check_win b clr] checks if there are 4 pieces with color [clr] in a row. *)
 val check_win : board -> color -> bool
 
-(** [empty_board b r c] is an empty board with [r] rows and [c] columns*)
+(** [empty_board b r c] creates an empty board with 6 rows and 7 columns, 
+    starting with the slot at row [r] and column [c] on initial board [b].*)
 val empty_board : board -> int -> int -> board
 
-(** [empty] is an empty list of boards *)
+(** [empty] is an empty board list *)
 val empty : board
 
-val drop_height : int -> board -> int
-
+(** [color_to_string clr] is the string representation of color [clr]. *)
 val color_to_string : color -> string
 
+(** [other_color clr] is [Black] if [clr] is [Red], and [Red] if [clr] is 
+    [Black]. *)
 val other_color : color -> color
