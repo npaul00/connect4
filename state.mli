65f8dc1886e93ff9143fbe45132803d71c68dba0
Nobonita Paul <nrp42@cornell.edu>
2019-11-05 19:18:27 -0500

    Documentation

--- state.mli
+++ state.mli
@@ -26,7 +26,7 @@ val display : board -> int -> unit
     column [c] in the current state [t]. *)
 val move : t -> int -> t
 
-(** [check_win b col] checks if there are four pieces with color [col] in a row.
+(** [check_win b clr] checks if there are four pieces with color [clr] in a row.
 *)
 val check_win : board -> color -> bool
 
@@ -36,8 +36,12 @@ val empty_board : board -> int -> int -> board
 (** [empty] is an empty list of boards *)
 val empty : board
 
+(** [drop_height col b] is the height of the piece being dropped in column [col]
+    in board [b]. *)
 val drop_height : int -> board -> int
 
+(** [color_to_string clr] is the String representation of the color type. *)
 val color_to_string : color -> string
 
+(** [other_color clr] is the color other than [clr]. *)
 val other_color : color -> color
